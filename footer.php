26a0430e151aa3bd633e64fe17d758a6a7bc955e
luofei614 <upfy@qq.com>
2012-10-29 15:32:47 +0800

    修正底部

--- footer.php
+++ footer.php
@@ -1,11 +1,3 @@
-<footer class="container mt20" role="contentinfo" id="colophon">
-                                        <div class="well clearfix" id="page-footer">
-                        <span class="credits alignleft">&copy; 2012 <a href="http://wp/blue/">第二个站点</a>, all rights reserved.</span>                        <div id="site-generator">
-                            <a rel="generator" target="_blank" title="Semantic Personal Publishing Platform" href="http://wordpress.org/">Proudly powered by WordPress</a>
-                        </div>
-                    </div><!-- #page-footer .well .clearfix -->
-</footer>
-
 <?php
 /**
  * Copyright (c) 2012 3g4k.com.
@@ -14,6 +6,15 @@
  * @author      luofei614 <weibo.com/luofei614>
  * @copyright   2012 3g4k.com.
  */
+?>
+<footer class="container mt20" role="contentinfo" id="colophon">
+                                        <div class="well clearfix" id="page-footer">
+                        <span class="credits alignleft">&copy; 2012 <a  href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>, all rights reserved.</span>                        <div id="site-generator">
+                            <a rel="generator" target="_blank" title="Gkwp is a wordpress theme framework" href="http://www.3g4k.com/">Proudly powered by Gkwp</a>
+                        </div>
+                    </div><!-- #page-footer .well .clearfix -->
+</footer>
+<?php
 wp_footer();
 do_action('gk_show_debug');//显示调试信息
 ?>
